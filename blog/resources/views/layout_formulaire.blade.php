713fcec1396e2d86f0a584e095d96d7f2d821ff7
“mathieu” <mathieu.joubert@live.fr>
2020-08-24 17:20:38 +0200

    :) ;)

--- blog/resources/views/layout_formulaire.blade.php
+++ blog/resources/views/layout_formulaire.blade.php
@@ -5,12 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/style.css">
-=======
-    <link href="../public/css/app.css" rel="stylesheet">
->>>>>>> mathieu
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
